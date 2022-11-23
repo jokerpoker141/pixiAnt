25b15bde5cdfe263bd3448ce2c4358e5683280c8
bruh <jokerpoker141@gmail.com>
2022-11-23 19:40:28 +0300

    Add project files.

---
+++ pixeiAnt0.1/FileRemember.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace pixeiAnt0._1
+{
+    class FileRemember : ListBox
+    {
+
+
+
+        public layout Layout    
+        {
+            get { return (layout)GetValue(ViewLayoutProperty); }
+            set { SetValue(ViewLayoutProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Layout.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewLayoutProperty =
+            DependencyProperty.Register("Layout", typeof(layout), typeof(FileRemember), new PropertyMetadata(layout.list));
+
+        // https://www.youtube.com/watch?v=SOD3hF1T49U tutorialindan gecmis dosyalari gormek icin takip et
+    }
+
+    public enum layout
+    {
+        Tile,
+        list
+    }
+}
